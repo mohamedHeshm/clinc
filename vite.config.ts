fa8ff0d29ff8a23d67c18c0f2af2c09d2b6c78a0
ENG | Mohamed <155893078+mohamedHeshm@users.noreply.github.com>
2026-09-14 13:57:25 +0300

    fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/clinc/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     port: 5173,
   },
